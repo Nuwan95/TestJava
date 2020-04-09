3310c4a5afcbe75c87266de3873253ebf3e8c50d
Nuwan95 <nuwanwanigasinha1995@gmail.com>
2020-04-09 21:15:28 +0530

    Block and control statement

---
+++ src/BlockTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by NUWAN WANIGASINGHA on 4/9/2020.
+ */
+public class BlockTest {
+    public static void main(String args[]) {
+        int x, y;
+        y = 20;
+        // the target of this loop is a block
+        for(x = 0; x<10; x++) {
+            System.out.println("This is x: " + x);
+            System.out.println("This is y: " + y);
+            y = y - 2;
+        }
+    }
+}
