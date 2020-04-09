482df7f2834d61d159b5896e227f5ef38cefd616
Nuwan95 <nuwanwanigasinha1995@gmail.com>
2020-04-09 17:56:33 +0530

    first

---
+++ src/Example1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by NUWAN WANIGASINGHA on 4/9/2020.
+ */
+public class Example1 {
+    // Your program begins with a call to main().
+    public static void main(String args[]) {
+        System.out.println("This is a simple Java program.");
+    }
+}
