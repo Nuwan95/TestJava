c594b8f12956933373e68329beb44c25116fe016
Nuwan95 <nuwanwanigasinha1995@gmail.com>
2020-04-18 14:55:58 +0530

    chapter2 commit1

---
+++ Src/Chapter2/CharDemo.java
@@ -0,0 +1,14 @@
+package Chapter2;
+
+/**
+ * Created by NUWAN WANIGASINGHA on 4/14/2020.
+ */
+public class CharDemo {
+    public static void main(String args[]) {
+        char ch1, ch2;
+        ch1 = 88; // code for X
+        ch2 = 'Y';
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
---
+++ Src/Chapter2/CharDemo2.java
@@ -0,0 +1,14 @@
+package Chapter2;
+
+/**
+ * Created by NUWAN WANIGASINGHA on 4/14/2020.
+ */
+public class CharDemo2 {
+    public static void main(String args[]) {
+        char ch1;
+        ch1 = 'X';
+        System.out.println("ch1 contains " + ch1);
+        ch1++; // increment ch1
+        System.out.println("ch1 is now " + ch1);
+    }
+}
